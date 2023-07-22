4971fc5d94e7e5edd2af0e0d325b6aa7d8146917
othegagan <gagankumar8711@gmail.com>
2023-07-22 14:39:18 +0530

    redone

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
     return (
         <>
-            <title>thegagan's portfolio</title>
+            <title>thegagan portfolio</title>
             <meta
                 content="width=device-width, initial-scale=1"
                 name="viewport"
